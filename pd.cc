867d5d2276a684bcf81d741de2d0a1d7b96fdc08
James Lee <mateddy@gmail.com>
2016-03-13 20:20:21 +0800

    Minor update

--- pd.cc
+++ pd.cc
@@ -8,8 +8,9 @@
 //#define DEBUG
 const unsigned int MIN_TURN = 100;
 const unsigned int MAX_TURN = 300;
+const unsigned int SINGLE = 15;
 
-void play(strategy* s, strategy* t, unsigned int turn = 15) {
+void play(strategy* s, strategy* t, unsigned int turn = SINGLE) {
   unsigned int m, n, k;
   bool a, b;
   vb u, v;
@@ -27,22 +28,32 @@ void play(strategy* s, strategy* t, unsigned int turn = 15) {
 }
 
 int main() {
-  unsigned int turn;
+  unsigned int turn, mode;
   std::vector<strategy*> ss =
     {new ALLC, new ALLD, new RAND, new TFT, new STFT, new TFTT, new TTFT, new GRIM, new PAVLOV};
   srand(static_cast<unsigned int>(time(NULL)));
 
-  // Each strategy play against each other twice, itself once
   turn = (unsigned int)rand() % (MAX_TURN - MIN_TURN) + MIN_TURN;
-#ifdef DEBUG
-  unsigned int i, j; scanf("%d %d", &i, &j); play(ss.at(i), ss.at(j));
-#else
-   
-  for (auto s: ss) for (auto t: ss) play(s, t, turn);
-  // Output and cleanup
-  for (auto s: ss) printf( "%.4f\n", s->score/2.0/ss.size() );
-#endif
-  
+
+  do {
+    printf("1: Round robin, %d-%d matches between each strategy\n", MIN_TURN, MAX_TURN);
+    printf("2: Single match, %d rounds\n", SINGLE);
+    printf("0: Quit\n");
+    scanf("%d", &mode);
+
+    switch (mode) {
+    case 1:
+      // Each strategy play against each other twice, itself once
+      for (auto s: ss) for (auto t: ss) play(s, t, turn);
+      for (auto s: ss) printf( "%s %.4f\n", typeid(*s).name(), s->score/2.0/ss.size() );
+      break;
+    case 2:
+      printf("Enter contestants: ");
+      unsigned int i, j; scanf("%d %d", &i, &j); play(ss.at(i), ss.at(j));
+    }
+
+  } while (mode);
+
   while (!ss.empty()) { delete ss.back(); ss.pop_back(); }
   
 }
